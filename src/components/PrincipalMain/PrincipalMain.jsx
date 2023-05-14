68c0e7d6de2ace8ac341e0f450c4072df8022ce3
zLuciel <82103687+zLuciel@users.noreply.github.com>
2023-05-13 20:05:08 -0500

    ready

--- src/components/PrincipalMain/PrincipalMain.jsx
+++ src/components/PrincipalMain/PrincipalMain.jsx
@@ -1,8 +1,8 @@
-//"use client"
-//import  { useRef, useEffect } from 'react';
-
+"use client"
+import  { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
 //import hoverEffect from 'hover-effect'
-
+import { Power3 } from 'gsap/dist/gsap';
 import Image from "next/legacy/image";
 import Vr from "/public/vr2.jpg"
 
@@ -14,8 +14,18 @@ import Robot from "public/robot.jpg"
 import "./css/PrincipalMain.css"
 
 const PrincipalMain = () => {
-
-
+  const imgRef = useRef(null);
+  const imgRef2 = useRef(null);
+  const titleRef = useRef(null);
+  const descriptionRef = useRef(null)
+ 
+  useEffect(() => {
+    // Animación con GSAP
+    gsap.from(imgRef.current, { opacity: 0, duration: 1.8, y: 250, ease: Power3.easeOut,delay:0.2});
+    gsap.from(imgRef2.current, { opacity: 0, duration: 1.8, y: 250, ease: Power3.easeOut,delay:0.8});
+    gsap.from(titleRef.current, { opacity: 0, duration: 1.8, x: -250, ease: Power3.easeOut,});
+    gsap.from(descriptionRef.current, { opacity: 0, duration: 1.8, x: -250, ease: Power3.easeOut,delay:0.2});
+  }, []);
   
   /*useEffect(() => {
       const Efectohover = async ()=>{
@@ -23,7 +33,7 @@ const PrincipalMain = () => {
         parent: document.querySelector('.img-1-main'),
         intensity: 0.2, // ajusta los valores según tus necesidades
         image1: "/vr1.jpg", // ruta a la primera imagen
-        image2: "/vr2.jpg" , // ruta a la segunda imagen
+        image2: "/dispar.jfif" , // ruta a la segunda imagen
         displacementImage: "/diss.png",
        imagesRatio: 1080 / 1920,
       });
@@ -34,18 +44,18 @@ const PrincipalMain = () => {
   
   return (
     <div className=" grid-main">
-      <div className="title-main">
+      <div className="title-main" ref={titleRef}>
       <h1>Vr Coggles</h1>
       </div>
 
-      <div className="text-info-main">
+      <div className="text-info-main" ref={descriptionRef}>
         <h3>
           IN AN INDUSTRY WHERE SEEMINGLY <p>EVERYTHING HAS</p> ALREADY BEEN SAID
           <span className="chine">ドセット</span>  <br /> SEEKS TO COMMENT MORE THAN STATE.
         </h3>
       </div>
 
-      <div className="img-1-main" >
+      <div className="img-1-main" ref={imgRef}>
       {<Image
        width={689}
        height={408}
@@ -55,7 +65,7 @@ const PrincipalMain = () => {
   />}
       </div>
 
-      <div className="img-2-main">
+      <div className="img-2-main" ref={imgRef2}>
       <Image
        width={438}
        height={658}
