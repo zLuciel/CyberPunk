de8b08f1ec2c39f50c0ba2c8c21ecb03e91f97fa
zLuciel <82103687+zLuciel@users.noreply.github.com>
2023-05-18 22:38:31 -0500

    ready

--- src/components/Informacion/Colmn2.jsx
+++ src/components/Informacion/Colmn2.jsx
@@ -1,81 +1,61 @@
 "use client";
-import { useRef } from "react";
+import { useRef} from "react";
 import "./css/Column2.css";
 import Image from "next/legacy/image";
-import amiga from "@/assets/amiga.png";
-import amiga2 from "@/assets/vr2.png";
 import EtiquetaSvg from "../EtiquetSvg/EtiquetaSvg";
-import { Carousel } from '@mantine/carousel';
+import { Carousel } from "@mantine/carousel";
 import Autoplay from "embla-carousel-autoplay";
-
+import Data from "./data.js";
 const Colmn2 = () => {
   const autoplay = useRef(Autoplay({ delay: 2000 }));
+
+
   return (
     <div className="container-col2 ">
       <h1 className="title-abs">Vr Coggles</h1>
 
       <div className="flex">
         <Carousel
-         plugins={[autoplay.current]}
-         onMouseEnter={autoplay.current.stop}
-         onMouseLeave={autoplay.current.reset}
+        className="Container-carusel"
+          plugins={[autoplay.current]}
+          onMouseEnter={autoplay.current.stop}
+          onMouseLeave={autoplay.current.reset}
           slideSize="100%"
+          slideGap="xl"
           align="center"
           loop
           slidesToScroll={1}
-          mx="auto"
+          
           withControls={false}
+          withIndicators
         >
-          <Carousel.Slide className="flex-img-swiper">
-            <div className="ContainerImg custom-cursor">
-              <div className="img-absolute">
-                <span>
-                  <EtiquetaSvg texto="80-A45" size={16} />
-                </span>
-                <div className="linea"></div>
-                <span>
-                  <EtiquetaSvg texto="407" size={16} />
-                </span>
-              </div>
+          {Data.map((product, i) => (
+            <Carousel.Slide key={i} className="flex-img-swiper">
+              <div className="ContainerImg custom-cursor">
+                <div className="img-absolute">
+                  <span>
+                    <EtiquetaSvg texto={product.serie} size={16} />
+                  </span>
+                  <div className="linea"></div>
+                  <span>
+                    <EtiquetaSvg texto={product.number} size={16} />
+                  </span>
+                </div>
 
-              <Image width={466} height={725} alt="nose" src={amiga} />
-            </div>
-          </Carousel.Slide>
-          <Carousel.Slide className="flex-img-swiper">
-            <div className="ContainerImg custom-cursor">
-              <div className="img-absolute">
-                <span>
-                  <EtiquetaSvg texto="80-A45" size={16} />
-                </span>
-                <div className="linea"></div>
-                <span>
-                  <EtiquetaSvg texto="407" size={16} />
-                </span>
+                <Image width={466} height={725} alt="nose" src={product.image} />
               </div>
+            </Carousel.Slide>
+          ))}
 
-              <Image width={466} height={725} alt="nose" src={amiga2} />
-            </div>
-          </Carousel.Slide>
         </Carousel>
 
-
-        <div className="ContainerFlex">
+      { <div className="ContainerFlex">
           <div className="flex-span-col2">
             <span>c.Future[1] </span>
             <span className="yellow">[ドセット]</span>
           </div>
 
-          <div className="flex-skew">
-            <span className="skew active"></span>
-            <span className="skew desactive"></span>
-            <span className="skew desactive"></span>
-            <span className="skew desactive"></span>
-            <span className="skew desactive"></span>
-            <span className="skew desactive"></span>
-            <span className="skew desactive"></span>
-          </div>
-        </div>
-
+          </div>}
         
       </div>
     </div>
---
+++ src/components/Informacion/data.js
@@ -0,0 +1,48 @@
+import amiga from "@/assets/amiga.png";
+import amiga2 from "@/assets/vr2.png";
+
+const Data = [
+    {
+        name:"c.Future[1]",
+        number:407,
+        serie:"80-A45",
+        image: amiga,
+    },
+    {
+        name:"c.Future[1]",
+        number:407,
+        serie:"80-A45",
+        image: amiga2,
+    },
+    {
+        name:"c.Future[1]",
+        number:407,
+        serie:"80-A45",
+        image: amiga,
+    },
+    {
+        name:"c.Future[1]",
+        number:407,
+        serie:"80-A45",
+        image: amiga2,
+    },
+    {
+        name:"c.Future[1]",
+        number:407,
+        serie:"80-A45",
+        image: amiga,
+    },
+    {
+        name:"c.Future[1]",
+        number:407,
+        serie:"80-A45",
+        image: amiga2,
+    },
+    {
+        name:"c.Future[1]",
+        number:407,
+        serie:"80-A45",
+        image: amiga,
+    },
+]
+export default Data
